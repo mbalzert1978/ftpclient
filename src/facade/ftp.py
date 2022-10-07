0e2af6860426bc84284aa0a7119b44d65870afc4
Markus Iorio <mbalzert@gmail.com>
2022-10-07 19:03:48 +0200

    fly

--- src/facade/ftp.py
+++ src/facade/ftp.py
@@ -1,18 +1,27 @@
 import ftplib
+from os import PathLike
 from pathlib import Path
 from ..config.ftpconfig import FTPConfig
 
 
 class FTPGetter:
-    def __init__(self, config: FTPConfig) -> None:
+    def __init__(
+        self, config: FTPConfig, remotefile: PathLike, localfile: PathLike
+    ) -> None:
         self.client = ftplib.FTP(config.host, config.user, config.password)
+        self.remotefile = Path(remotefile)
+        self.localfile = Path(localfile)
 
-    def execute(self, remotefile: Path, localfile: Path) -> str:
-        start = 0 if not remotefile.is_absolute() else 1
-        end = len(remotefile.parts) if not remotefile.suffix else -1
-        for folder in remotefile.parts[start:end]:
+    def execute(self) -> str:
+        for folder in self.remotefile.parts[self.start() : self.end()]:
             self.client.cwd(folder)
-        with localfile.open("rb") as local:
+        with self.localfile.open("wb") as local:
             return self.client.retrbinary(
-                "RETR %s" % remotefile.name, local.write
+                "RETR %s" % self.remotefile.name, local.write
             )
+
+    def end(self) -> int:
+        return len(self.remotefile.parts) if not self.remotefile.suffix else -1
+
+    def start(self) -> int:
+        return 0 if not self.remotefile.is_absolute() else 1
